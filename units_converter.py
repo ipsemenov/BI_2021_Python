4ae50921ccb8cda706ac8bc52340d6ce9ab8fb2b
Ivan Semenov <isemenov.bioinfo1998@gmail.com>
2021-10-19 11:38:44 +0300

    add help message and T availability

--- units_converter.py
+++ units_converter.py
@@ -12,6 +12,14 @@ def is_number_func(str):
 
 
 if __name__ == "__main__":
+    print('''
+    This is a prorgam for temperature convertation into different units!
+    Available T units: celsius, kelvin, fahrenheit
+    Units input: T units you have
+    Units output: T units you want to get
+    Value to convert: T value to convert from units unput to units output\n
+    If you want to exit program, just enter "exit" in any line.\n
+    ''')
     available_units = {'celsius', 'kelvin', 'fahrenheit'}
     units_abbr = {'celsius': 'C', 'kelvin': 'K', 'fahrenheit': 'F'}
     while True:
@@ -39,6 +47,18 @@ if __name__ == "__main__":
             if value == 'exit':
                 break
         value = float(value)
+        if units_input == 'celsius' and value < -273.15:
+            print('''
+            WARNING: Convertation still will be done
+            But please note that T cannot be less than -273.15 C!\n''')
+        elif units_input == 'kelvin' and value < 0:
+            print('''
+            WARNING: Convertation still will be done
+            But please note that T cannot be less than 0 K!\n''')
+        elif units_input == 'fahrenheit' and value < -459.67:
+            print('''
+            WARNING: Convertation still will be done
+            But please note that T cannot be less than -459.67 F!\n''')
         if units_input == 'celsius':
             if units_output == 'kelvin':
                 res = value + 273.15
